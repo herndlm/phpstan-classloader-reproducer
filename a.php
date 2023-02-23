be5ff999eacdecb32bef2a048acf1629529d1563
Martin Herndl <martin.herndl@icis.com>
2023-02-23 14:28:02 +0100

    Initial commit

---
+++ a.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+use Herndlm\PhpstanClassloaderReproducer\FooInterface;
+
+define('FOO', 'foo');
+
+/** @var FooInterface $foo */
+$foo = null;
---
+++ b.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+define('FOO', 'foo');
+
+use Herndlm\PhpstanClassloaderReproducer\FooInterface;
+
+/** @var FooInterface $foo */
+$foo = null;
